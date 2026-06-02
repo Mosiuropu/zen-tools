40de8dbe7f34da8e338aeab9960b7612c5e6468f
Mosiur <Opu128@gmail.com>
2026-06-02 09:58:10 +0600

    debug: add JS loaded log

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+console.log('Zen Tools: JS Loaded');
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
